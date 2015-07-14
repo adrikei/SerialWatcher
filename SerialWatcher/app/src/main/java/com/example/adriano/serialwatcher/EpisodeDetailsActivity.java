d92674ff915a723bc318288e42aaccfc0a0e562f
Adriano Rodrigues <adrikei@gmail.com>
2015-07-14 13:06:37 -0300

    episode details activity

---
+++ SerialWatcher/app/src/main/java/com/example/adriano/serialwatcher/EpisodeDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.adriano.serialwatcher;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class EpisodeDetailsActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.episode_details_activity);
+    }
+}
